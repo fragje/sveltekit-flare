6caab59b7c5c07a6ecd909ae7608098ddbcbc9e7
Frank Gjertsen <frank@frontkom.com>
2023-03-05 22:46:33 +0100

    Output platform

--- src/routes/request/+server.js
+++ src/routes/request/+server.js
@@ -1,6 +1,8 @@
 import { json } from '@sveltejs/kit';
 
-export function GET({ request, platform }) {
+export async function GET({ request, platform }) {
 
-  return json({...request, ...platform});
+  // const response = await request.env.FLARE('count');
+  
+  return json(platform);
 }
